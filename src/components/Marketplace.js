c961f62db6d85c7a6af5212c7a87d65c83ebfc52
eldor47 <eldor474747@gmail.com>
2022-09-27 01:37:14 -0700

    Adds styling fixes

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -1,6 +1,26 @@
 import '../styles/Marketplace.css'
+import React from 'react'
+import axios from "axios";
 
 function Marketplace({account, contracts}) {
+
+  const [data, setData] = React.useState([]);
+  const [error, setError] = React.useState();
+
+  React.useEffect(() => {
+    getListingData()
+  }, []);
+
+  const getListingData = () => {
+    let url = process.env.REACT_APP_BASE_URI_NEW + '/getListings';
+    
+    axios.post(url, {}).then((response) => {
+      // Set data and viewable data for scrollable component
+      console.log(response.data)
+      setData(response.data.listings)
+    });
+  }
+
   return (
     <div className="marketplace">
       <div className="left-market">
@@ -22,222 +42,21 @@ function Marketplace({account, contracts}) {
         </div>
       </div>
       <div className="right-market">
-        <div className="market-item">
-          <img className="item-img" src="https://i1.wp.com/www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg?ssl=1"></img>
+      {data.map(e => {
+        return (<div className="market-item" key={e.id}>
+          <img className="item-img" src={e.image}></img>
           <div className='item-body'>
-            <p className='description'>4k beautiful landscape banners based project</p>
+            <p className='description'>{e.description}</p>
             <p className='expiration'>Ends in 2 days</p>
           </div>
           <div>
-          <a href="https://twitter.com/baebaecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/twitter.png"/></a>
-            <a href="https://discord.gg/baecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/discord.png"/></a>
+            <a href={e.twitter}><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/twitter.png"/></a>
+            <a href={e.discord}><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/discord.png"/></a>
           </div>
-          <button className='button-submit'>View Whitelist</button>
-        </div>
-        <div className="market-item">
-          <img className="item-img" src="https://i1.wp.com/www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg?ssl=1"></img>
-          <div className='item-body'>
-            <p className='description'>4k beautiful landscape banners based project</p>
-            <p className='expiration'>Ends in 2 days</p>
-          </div>
-          <div>
-          <a href="https://twitter.com/baebaecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/twitter.png"/></a>
-            <a href="https://discord.gg/baecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/discord.png"/></a>
-          </div>
-          <button className='button-submit'>View Whitelist</button>
-        </div>
-        <div className="market-item">
-          <img className="item-img" src="https://i1.wp.com/www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg?ssl=1"></img>
-          <div className='item-body'>
-            <p className='description'>4k beautiful landscape banners based project</p>
-            <p className='expiration'>Ends in 2 days</p>
-          </div>
-          <div>
-          <a href="https://twitter.com/baebaecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/twitter.png"/></a>
-            <a href="https://discord.gg/baecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/discord.png"/></a>
-          </div>
-          <button className='button-submit'>View Whitelist</button>
-        </div>
-        <div className="market-item">
-          <img className="item-img" src="https://i1.wp.com/www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg?ssl=1"></img>
-          <div className='item-body'>
-            <p className='description'>4k beautiful landscape banners based project</p>
-            <p className='expiration'>Ends in 2 days</p>
-          </div>
-          <div>
-          <a href="https://twitter.com/baebaecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/twitter.png"/></a>
-            <a href="https://discord.gg/baecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/discord.png"/></a>
-          </div>
-          <button className='button-submit'>View Whitelist</button>
-        </div>
-        <div className="market-item">
-          <img className="item-img" src="https://i1.wp.com/www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg?ssl=1"></img>
-          <div className='item-body'>
-            <p className='description'>4k beautiful landscape banners based project</p>
-            <p className='expiration'>Ends in 2 days</p>
-          </div>
-          <div>
-          <a href="https://twitter.com/baebaecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/twitter.png"/></a>
-            <a href="https://discord.gg/baecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/discord.png"/></a>
-          </div>
-          <button className='button-submit'>View Whitelist</button>
-        </div>
-        <div className="market-item">
-          <img className="item-img" src="https://i1.wp.com/www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg?ssl=1"></img>
-          <div className='item-body'>
-            <p className='description'>4k beautiful landscape banners based project</p>
-            <p className='expiration'>Ends in 2 days</p>
-          </div>
-          <div>
-          <a href="https://twitter.com/baebaecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/twitter.png"/></a>
-            <a href="https://discord.gg/baecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/discord.png"/></a>
-          </div>
-          <button className='button-submit'>View Whitelist</button>
-        </div>
-        <div className="market-item">
-          <img className="item-img" src="https://i1.wp.com/www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg?ssl=1"></img>
-          <div className='item-body'>
-            <p className='description'>4k beautiful landscape banners based project</p>
-            <p className='expiration'>Ends in 2 days</p>
-          </div>
-          <div>
-          <a href="https://twitter.com/baebaecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/twitter.png"/></a>
-            <a href="https://discord.gg/baecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/discord.png"/></a>
-          </div>
-          <button className='button-submit'>View Whitelist</button>
-        </div>
-        <div className="market-item">
-          <img className="item-img" src="https://i1.wp.com/www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg?ssl=1"></img>
-          <div className='item-body'>
-            <p className='description'>4k beautiful landscape banners based project</p>
-            <p className='expiration'>Ends in 2 days</p>
-          </div>
-          <div>
-          <a href="https://twitter.com/baebaecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/twitter.png"/></a>
-            <a href="https://discord.gg/baecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/discord.png"/></a>
-          </div>
-          <button className='button-submit'>View Whitelist</button>
-        </div>
-        <div className="market-item">
-          <img className="item-img" src="https://i1.wp.com/www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg?ssl=1"></img>
-          <div className='item-body'>
-            <p className='description'>4k beautiful landscape banners based project</p>
-            <p className='expiration'>Ends in 2 days</p>
-          </div>
-          <div>
-          <a href="https://twitter.com/baebaecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/twitter.png"/></a>
-            <a href="https://discord.gg/baecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/discord.png"/></a>
-          </div>
-          <button className='button-submit'>View Whitelist</button>
-        </div>
-        <div className="market-item">
-          <img className="item-img" src="https://i1.wp.com/www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg?ssl=1"></img>
-          <div className='item-body'>
-            <p className='description'>4k beautiful landscape banners based project</p>
-            <p className='expiration'>Ends in 2 days</p>
-          </div>
-          <div>
-          <a href="https://twitter.com/baebaecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/twitter.png"/></a>
-            <a href="https://discord.gg/baecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/discord.png"/></a>
-          </div>
-          <button className='button-submit'>View Whitelist</button>
-        </div>
-        <div className="market-item">
-          <img className="item-img" src="https://i1.wp.com/www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg?ssl=1"></img>
-          <div className='item-body'>
-            <p className='description'>4k beautiful landscape banners based project</p>
-            <p className='expiration'>Ends in 2 days</p>
-          </div>
-          <div>
-          <a href="https://twitter.com/baebaecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/twitter.png"/></a>
-            <a href="https://discord.gg/baecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/discord.png"/></a>
-          </div>
-          <button className='button-submit'>View Whitelist</button>
-        </div>
-        <div className="market-item">
-          <img className="item-img" src="https://i1.wp.com/www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg?ssl=1"></img>
-          <div className='item-body'>
-            <p className='description'>4k beautiful landscape banners based project</p>
-            <p className='expiration'>Ends in 2 days</p>
-          </div>
-          <div>
-          <a href="https://twitter.com/baebaecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/twitter.png"/></a>
-            <a href="https://discord.gg/baecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/discord.png"/></a>
-          </div>
-          <button className='button-submit'>View Whitelist</button>
-        </div>
-        <div className="market-item">
-          <img className="item-img" src="https://i1.wp.com/www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg?ssl=1"></img>
-          <div className='item-body'>
-            <p className='description'>4k beautiful landscape banners based project</p>
-            <p className='expiration'>Ends in 2 days</p>
-          </div>
-          <div>
-          <a href="https://twitter.com/baebaecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/twitter.png"/></a>
-            <a href="https://discord.gg/baecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/discord.png"/></a>
-          </div>
-          <button className='button-submit'>View Whitelist</button>
-        </div>
-        <div className="market-item">
-          <img className="item-img" src="https://i1.wp.com/www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg?ssl=1"></img>
-          <div className='item-body'>
-            <p className='description'>4k beautiful landscape banners based project</p>
-            <p className='expiration'>Ends in 2 days</p>
-          </div>
-          <div>
-          <a href="https://twitter.com/baebaecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/twitter.png"/></a>
-            <a href="https://discord.gg/baecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/discord.png"/></a>
-          </div>
-          <button className='button-submit'>View Whitelist</button>
-        </div>
-        <div className="market-item">
-          <img className="item-img" src="https://i1.wp.com/www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg?ssl=1"></img>
-          <div className='item-body'>
-            <p className='description'>4k beautiful landscape banners based project</p>
-            <p className='expiration'>Ends in 2 days</p>
-          </div>
-          <div>
-          <a href="https://twitter.com/baebaecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/twitter.png"/></a>
-            <a href="https://discord.gg/baecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/discord.png"/></a>
-          </div>
-          <button className='button-submit'>View Whitelist</button>
-        </div>
-        <div className="market-item">
-          <img className="item-img" src="https://i1.wp.com/www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg?ssl=1"></img>
-          <div className='item-body'>
-            <p className='description'>4k beautiful landscape banners based project</p>
-            <p className='expiration'>Ends in 2 days</p>
-          </div>
-          <div>
-          <a href="https://twitter.com/baebaecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/twitter.png"/></a>
-            <a href="https://discord.gg/baecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/discord.png"/></a>
-          </div>
-          <button className='button-submit'>View Whitelist</button>
-        </div>
-        <div className="market-item">
-          <img className="item-img" src="https://i1.wp.com/www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg?ssl=1"></img>
-          <div className='item-body'>
-            <p className='description'>4k beautiful landscape banners based project</p>
-            <p className='expiration'>Ends in 2 days</p>
-          </div>
-          <div>
-          <a href="https://twitter.com/baebaecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/twitter.png"/></a>
-            <a href="https://discord.gg/baecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/discord.png"/></a>
-          </div>
-          <button className='button-submit'>View Whitelist</button>
-        </div>
-        <div className="market-item">
-          <img className="item-img" src="https://i1.wp.com/www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg?ssl=1"></img>
-          <div className='item-body'>
-            <p className='description'>4k beautiful landscape banners based project</p>
-            <p className='expiration'>Ends in 2 days</p>
-          </div>
-          <div>
-          <a href="https://twitter.com/baebaecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/twitter.png"/></a>
-            <a href="https://discord.gg/baecafe"><img className='header-icon' src="https://dx8cklxaufs1v.cloudfront.net/baecafeweb/image/discord.png"/></a>
-          </div>
-          <button className='button-submit'>View Whitelist</button>
-        </div>
+          <button className='button-submit'>View</button>
+          <button className='button-submit'>Buy</button>
+        </div>)
+        })}
       </div>
     </div>
   );
